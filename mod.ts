cca17dd064ade73c1289d9068979bc43ad7dfb82
Axel Obscura <axosar@gmail.com>
2020-07-20 09:58:37 -0500

    Working with Middleware

--- mod.ts
+++ mod.ts
@@ -3,8 +3,34 @@ import { Application } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
+app.use(async (ctx, next) => {
+    await next();
+    const time = ctx.response.headers.get('X-Response-Time');
+    console.log(`${ctx.request.method} - ${ctx.request.url}: ${time}`);
+});
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const delta = Date.now() - start;
+    ctx.response.headers.set('X-Response-Time', `${delta}ms it took to respond...`);
+});
+
 app.use((ctx) => {
-    ctx.response.body = `NASA MISSION CONTROL API`;
+    ctx.response.body = `
+               ___
+     |     | |
+    / \    | |
+   |--o|===|-|
+   |---|   |d|
+  /     \  |w|
+ | U     | |b|
+ | S     |=| |
+ | A     | | |
+ |_______| |_|
+  |@| |@|  | |
+___________|_|_
+    `;
 });
 
 if (import.meta.main) {
