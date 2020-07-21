3c98ce66a6e0587b013ff5f8ffeb9b05e52ee89d
Axel Obscura <axosar@gmail.com>
2020-07-21 08:24:53 -0500

    Oak ROUTER

--- mod.ts
+++ mod.ts
@@ -1,58 +1,45 @@
 import { Application, send } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 
+import api from "./api.ts";
+
 const app = new Application();
 const PORT = 8000;
 
 app.use(async (ctx, next) => {
-    await next();
-    const time = ctx.response.headers.get('X-Response-Time');
-    console.log(`${ctx.request.method} - ${ctx.request.url}: ${time}`);
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} - ${ctx.request.url}: ${time}`);
 });
 
 app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const delta = Date.now() - start;
-    ctx.response.headers.set('X-Response-Time', `${delta}ms it took to respond...`);
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set(
+    "X-Response-Time",
+    `${delta}ms it took to respond...`,
+  );
 });
+
+app.use(api.routes());
 
 app.use(async (ctx) => {
-    const filePath = ctx.request.url.pathname;
-    const fileWhiteList = [
-        "/index.html",
-        "/javascript/script.js",
-        "/images/favicon.png",
-        "/stylesheets/style.css"
-    ];
-    if (fileWhiteList.includes(filePath)) {
-        await send(ctx, filePath, {
-            root: `${Deno.cwd()}/public`
-        });
-    }
-
-});
-
-app.use((ctx) => {
-    ctx.response.body = `
-               ___
-     |     | |
-    / \    | |
-   |--o|===|-|
-   |---|   |d|
-  /     \  |w|
- | U     | |b|
- | S     |=| |
- | A     | | |
- |_______| |_|
-  |@| |@|  | |
-___________|_|_
-    `;
+  const filePath = ctx.request.url.pathname;
+  const fileWhiteList = [
+    "/index.html",
+    "/javascript/script.js",
+    "/images/favicon.png",
+    "/stylesheets/style.css",
+  ];
+  if (fileWhiteList.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    });
+  }
 });
 
 if (import.meta.main) {
-    await app.listen({
-        port: PORT
-    });
+  await app.listen({
+    port: PORT,
+  });
 }
-
-
