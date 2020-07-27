36ff137d59ca7c01de88f34761e77f6885a80974
Axel Obscura <axosar@gmail.com>
2020-07-27 14:05:33 -0500

    Launches

--- api.ts
+++ api.ts
@@ -1,6 +1,7 @@
 import { Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 
 import * as planets from "./models/planets.ts";
+import * as launches from "./models/launches.ts";
 
 const router = new Router();
 
@@ -23,6 +24,21 @@ ___________|_|_
 
 router.get("/planets", (ctx) => {
   ctx.response.body = planets.getAllPlanets();
+});
+
+router.get("/launches", (ctx) => {
+  ctx.response.body = launches.getAll();
+});
+
+router.get("/launches/:id", (ctx) => {
+  if (ctx.params?.id) {
+    const launchesList = launches.getOne(Number(ctx.params.id));
+    if (launchesList) {
+      ctx.response.body = launchesList;
+    } else {
+      ctx.throw(400, "Launch with that ID does not exists...");
+    }
+  }
 });
 
 export default router;
